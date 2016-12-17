31aa06252318fd07f01477d6d400cd4fc308de62
LeeZz <42920782@qq.com>
2016-12-17 16:36:13 +0800

    numpy attribute demo

---
+++ np_attribute.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# create numpy array
+array = np.array([[1,2,3],
+					[4,5,6]])
+
+print array
+# numpy dimiation
+print array.ndim
+# numpy data type
+print array.dtype
+# numpy data shape
+print array.shape
+# numpy data size
+print array.size
